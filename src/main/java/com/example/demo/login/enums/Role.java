f7747b0f3d91a529ee610e976a9a58d422b1c8e3
Yeom <a1004@Yeoms-MacBook-Air.local>
2024-01-18 17:00:34 +0900

    feat: google Login

---
+++ src/main/java/com/example/demo/login/enums/Role.java
@@ -0,0 +1,14 @@
+package com.example.demo.login.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+    BUYER("ROLE_BUYER","구매자"),
+    ARTIST("ROLE_ARTIST","아티스트");
+
+    private final String key;
+    private final String title;
+}
---
+++ src/main/java/com/example/demo/login/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.login.repository;
+
+import com.example.demo.login.dto.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
