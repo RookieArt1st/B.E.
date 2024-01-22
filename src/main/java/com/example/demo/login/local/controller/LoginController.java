e83f0f84c97d625ad7ad724f943ecce2f5626d4c
Jongseok Kwon <jjijji5728@tukorea.ac.kr>
2024-01-23 02:51:57 +0900

    feat: 로그인 후 세션 저장 및 로그아웃시 세션 종료 기능

--- src/main/java/com/example/demo/login/local/controller/LoginController.java
+++ src/main/java/com/example/demo/login/local/controller/LoginController.java
@@ -3,10 +3,10 @@ package com.example.demo.login.local.controller;
 import com.example.demo.login.local.dto.LoginRequestDto;
 import com.example.demo.login.local.dto.ResponseDto;
 import com.example.demo.login.local.service.JoinService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,17 +19,38 @@ public class LoginController {
     private final JoinService joinService;
 
     @PostMapping("/check")
-    public ResponseEntity<ResponseDto> check(@RequestBody LoginRequestDto dto) {
+    public ResponseEntity<ResponseDto> check(@RequestBody LoginRequestDto dto, HttpSession session) {
         ResponseEntity<ResponseDto> responseEntity = joinService.find(dto);
 
         // 성공
         if (responseEntity.getStatusCode().is2xxSuccessful()) {
             log.info("로그인에 성공했습니다.");
+
+            // 세션 정보에 로그인 저장
+            session.setAttribute("loginUser", responseEntity.getBody());
+
+            // 세션 정보 체크
+            ResponseDto loginUser = (ResponseDto) session.getAttribute("loginUser");
+            log.info("세션에 저장된 사용자 정보: {}", loginUser.getUsername());
+
             return ResponseEntity.ok(responseEntity.getBody());
         }
 
         log.info("로그인에 실패했습니다.");
         // 실패
         return ResponseEntity.badRequest().body(responseEntity.getBody());
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(HttpSession session) {
+        // 세션 만료시키기
+        session.invalidate();
+
+        // 로그아웃 후 세션에 저장된 정보 출력
+        //log.info(String.valueOf(session.getAttributeNames()));
+        //ResponseDto loginUser = (ResponseDto) session.getAttribute("loginUser");
+        //log.info("로그아웃 후 세션에 저장된 사용자 정보: {}", loginUser);
+
+        return ResponseEntity.ok("로그아웃 되었습니다.");
     }
 }
